adc97ce4761666fa614443280e1109c857688878
Adit <adit@studyinasia.co>
2026-06-23 23:34:46 +0700

    style: :lipstick: Update page loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import PromptForm from "@/app/components/PromptPage";
+import { LoadingSpinner } from "./components/LoadingSpinner";
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
 function PromptFormSkeleton() {
   return (
     <div className="w-full">
-      <div className="h-32 w-full animate-pulse rounded-lg border border-zinc-300 bg-zinc-100 dark:border-zinc-600 dark:bg-zinc-800" />
+      <LoadingSpinner />
     </div>
   );
 }
